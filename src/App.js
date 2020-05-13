a722078af6fd15bcc40f0ce35d155e8a87a0c30c
Brendan Eggers <brendan.eggers@openslate.com>
2020-05-13 16:20:29 -0400

    Adding in fetch stuff for FE just to make sure BE was working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const fetchData = async () => {
+    const req = await fetch(`https://eggers-dev.herokuapp.com/jobs`);
+    const response = await req.json();
+  };
+  fetchData();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>HELLO WORLD</h1>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+
+}));
+
+const Header = () => {
+  const classes = useStyles();
+	return (
+    <>
+		  <h1>Header Component</h1>
+    </>
+	);
+};
+
+export default Header;
