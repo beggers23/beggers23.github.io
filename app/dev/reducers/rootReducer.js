7e93cba280a348a32e705b8dc7c4c8225dd5bd78
Brendan Eggers <eggers.brendan@gmail.com>
2020-05-13 09:06:18 -0400

    Adding mono repo structure

--- app/dev/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import rootSessionReducer from './session.js';
-
-const appStore = combineReducers({
-	session: rootSessionReducer,
-});
-
-export default appStore;
